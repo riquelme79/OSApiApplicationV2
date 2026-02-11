c2c43b6a8c596660fb6cac4c0d3d61fde421ded9
Riquelme <rm7629586@gmail.com>
2026-02-11 17:10:01 -0300

    Update final quinta fase

--- src/main/java/br/dev/riquelme/OSApiApplication/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/br/dev/riquelme/OSApiApplication/api/exceptionhandler/ApiExceptionHandler.java
@@ -2,9 +2,13 @@
 package br.dev.riquelme.OSApiApplication.api.exceptionhandler;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -15,7 +19,8 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     
     
     @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
             HttpHeaders headers,
             HttpStatusCode status,
             WebRequest request) {
@@ -25,7 +30,19 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
         problema.setTitulo("Um ou mais campos inválidos! Tente novamente.");
         problema.setDataHora(LocalDateTime.now());
         
+        List<ProblemaException.CampoProblema> camposComErro = new ArrayList<ProblemaException.CampoProblema>();
+        
+        for (ObjectError error : ex.getBindingResult().getAllErrors()) {
+            String nomeCampo = ((FieldError) error).getField();
+            String mensagemCampo = error.getDefaultMessage();
+            
+            camposComErro.add( new ProblemaException.CampoProblema(nomeCampo, mensagemCampo));
+            
+        }
+        
+        problema.setCampos(camposComErro);
+        
         return super.handleExceptionInternal(ex, problema, headers, status, request);
     }
-
+    
 }
